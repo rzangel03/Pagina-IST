906f82f178ce4c6f5e460fe2f5beabce3d69b804
rzangel03 <32023147+rzangel03@users.noreply.github.com>
2017-09-17 18:35:04 -0500

    adew

--- IST/js/menu.js
+++ IST/js/menu.js
@@ -1,5 +1,17 @@
-var btnmenu=document.getElementById('btn-menu');
-var nav = document.getElementById('nav');
-btnmenu.addEventListener('click',function(){
-    nav.classList.toggle('mostrar');
+$(document).ready(function() {
+  $('.menu li:has(ul)').click(function(event) {
+    event.preventDefault();
+    if ($(this).hasClass('activado')) {
+      $(this).removeClass('activado');
+      $(this).children('ul').slideUp();
+    } else {
+      $('.menu li ul').slideUp();
+      $('.menu li').removeClass('activado');
+      $(this).addClass('activado');
+      $(this).children('ul').slideDown();
+    }
+  });
+    $('.btn-menu').click(function(){
+        $('.contenedor-menu .menu').slideToggle();
+    })
 });
